8904da58bc29079aa6fe94693dd7458dcf15c8cb
Özhan Mavili <ozhan_mavili@epam.com>
2023-10-25 00:15:12 +0300

    init

---
+++ src/assets/scripts/common.js
@@ -0,0 +1,11 @@
+console.log('>> Common script is loaded.');
+
+
+// Import only the Bootstrap components we need
+import { Popover } from 'bootstrap';
+
+// Create an example popover
+document.querySelectorAll('[data-bs-toggle="popover"]')
+  .forEach(popover => {
+    new Popover(popover)
+  })
