1bd2f95bc5bf9539f489fce2a237d7959f543f25
Özhan Mavili <ozhanmavili@gmail.com>
2023-11-12 23:53:33 +0300

    Checkout shipment and payment steps

--- src/views/pages/checkout/checkout.js
+++ src/views/pages/checkout/checkout.js
@@ -1,3 +1,5 @@
+import * as $ from 'jquery';
+window['jQuery'] = window['$'] = $;
 import Swiper from 'swiper';
 import { Pagination } from 'swiper/modules';
 
@@ -9,3 +11,19 @@ const productTileSwiper = new Swiper('.js-product-tile-swiper', {
     clickable: true,
   },
 });
+
+$(window).on('load', function () {
+  $('.js-payment-type-select').on('click', function () {
+    $('.js-payment-type').removeClass('selected');
+    $(this).closest('.js-payment-type').addClass('selected');
+  });
+
+  $('.js-same-address').on('change', function () {
+    if($(this).is(':checked')) {
+      $('.js-billing-address').addClass('d-none');
+    } else {
+      $('.js-billing-address').removeClass('d-none');
+    }
+  });
+});
+
